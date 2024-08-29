95e63e72df9ee7b58bfbee306bbb66aaf736fa90
jrpompio <junior.ruiz@ucr.ac.cr>
2024-08-28 22:50:01 -0600

    tengo que moverme de pic :c
    Signed-off-by: jrpompio <junior.ruiz@ucr.ac.cr>

--- LAB1/scr/lab1.c
+++ LAB1/scr/lab1.c
@@ -1,6 +1,6 @@
 #include <pic14/pic12f675.h>
 
-#define time 1
+// #define time 1
 
 int __at 0x2007 __config =
  	_CP_OFF & _CPD_OFF            // Desactiva protecciones de codigo y de EEPROM
@@ -11,21 +11,26 @@ int __at 0x2007 __config =
 
 void delay(unsigned int tiempo);
 void display(unsigned int valor);
+int capturar(void);
 
 void main(void)
 {
     ANSEL = 0x00;         // Configurando pines como digitales
     CMCON = 0x07;         // Desactivando comparadores
     TRISIO = 0b00000000;  // 
-    GPIO &= (0b00000000); // Inicializa todos los pines en bajo
+    GPIO &= (0b00000000); // Inicializa todos los pines en bajo 
     unsigned int aleatorio = 99;
     while (1)
-    { 
-      aleatorio = aleatorio - 1;
-      display(aleatorio);
-
+    {   
+        display(aleatorio);
+        delay(500);           // quitar delay para ser aleatorio
+        if (aleatorio == 0){
+          aleatorio = 100;
+        }
+        aleatorio--;
     }
 }
+
 
 void delay(unsigned int tiempo)
 {
@@ -38,26 +43,23 @@ void delay(unsigned int tiempo)
 
 void display(unsigned int valor){
     unsigned short int semiCiclo = 16;     // Se necesitan enviar 8 bits
+    unsigned short int bit_enviar = 0;     // Se necesitan enviar 8 bits
     GP0 = 0;
     int decenas = valor / 10; 
     int unidades = valor % 10;
     decenas = (decenas << 4);
     valor = decenas | unidades;
     while (semiCiclo > 0)
-    // DEBO LIMPIAR ESTA COCHINADA
-    
     { 
       if (GP0 == 0){
-        GP1 = ((valor << 
-        ((16-semiCiclo)/2)
-        )  // Se obtiene el digito más significativo
-         & 0x80) >> 7;                // se shiftea 7 veces 
-//        bit_enviar = bit_enviar + 1;
+        GP1 = ((valor >> bit_enviar)  // Se obtiene el digito más significativo
+         & 0x01);                // se shiftea 7 veces 
+        bit_enviar++;
         delay(1);                     // delay de seguridad para que
                                       // no se guarde el digito antes de cambiarlo
       }
       GP0 = ~GP0;
-      semiCiclo = semiCiclo - 1;
+      semiCiclo--;
       delay(1);
     }
 }
