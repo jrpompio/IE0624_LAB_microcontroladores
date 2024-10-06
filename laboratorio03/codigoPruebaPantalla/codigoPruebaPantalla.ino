a9110fec6dc54294989aca4de11fce2112dee182
Rocha0104 <mariorocha200104@gmail.com>
2024-10-06 16:49:32 -0600

    Funcion para mostrar imagen en pantalla y pruebas
    Signed-off-by: Rocha0104 <mariorocha200104@gmail.com>

--- laboratorio03/codigoPruebaPantalla/codigoPruebaPantalla.ino
+++ laboratorio03/codigoPruebaPantalla/codigoPruebaPantalla.ino
@@ -12,42 +12,70 @@
 Adafruit_PCD8544 display = Adafruit_PCD8544(CLK_PIN, DIN_PIN, DC_PIN, CE_PIN, RST_PIN);
 
 void setup() {
-static int contador = 0;
+  // Inicializar la pantalla
+  display.begin();
+  display.setContrast(50);   // Ajustar el contraste de la pantalla
 
   // Limpiar pantalla
   display.clearDisplay();
+  display.setTextSize(0.1);
 
-  // Escribir el contador
+  // Escribir texto
+  display.setTextColor(BLACK);
   display.setCursor(0, 0);
-  display.print("Contador: ");
-  display.print(contador);
+  display.println("Hola!");
+  display.println("PCD8544");
 
-  // Mostrar en pantalla
+  // Mostrar cambios en pantalla
   display.display();
-
-  // Incrementar contador
-  contador++;
-
-  // Esperar 1 segundo
-  delay(1000);
 }
 
-void loop() {
-  int sensorValue = analogRead(A0);  // Leer un sensor en A0
-  float voltage = sensorValue * (5.0 / 1023.0); // Convertir a voltaje
-
+void screen(float voltage0, float voltage1, float voltage2, float voltage3){
   // Limpiar pantalla
   display.clearDisplay();
 
   // Mostrar el valor del sensor
   display.setCursor(0, 0);
-  display.print("Voltaje: ");
-  display.print(voltage);
+  display.print("input0: ");
+  display.print(voltage0);
   display.println("V");
+
+  display.setCursor(0, 10);
+  display.print("input1: ");
+  display.print(voltage1);
+  display.println("V");
+
+  display.setCursor(0, 20);
+  display.print("input2: ");
+  display.print(voltage2);
+  display.println("V");
+
+  display.setCursor(0, 30);
+  display.print("input3: ");
+  display.print(voltage3);
+  display.println("V");
+
+  display.setCursor(0, 41);
+  display.print("Vmax: 24V");
 
   // Mostrar en pantalla
   display.display();
 
+  }
+
+void loop() {
+  int sensorValue0 = analogRead(A0);  // Leer tension en A0
+  int sensorValue1 = analogRead(A1);  // Leer tension en A1
+  int sensorValue2 = analogRead(A2);  // Leer tension en A2
+  int sensorValue3 = analogRead(A3);  // Leer tension en A3
+  float voltage0 = sensorValue0 * (5.0 / 1023.0); // Convertir a voltaje
+  float voltage1 = sensorValue1 * (5.0 / 1023.0); // Convertir a voltaje
+  float voltage2 = sensorValue2 * (5.0 / 1023.0); // Convertir a voltaje
+  float voltage3 = sensorValue3 * (5.0 / 1023.0); // Convertir a voltaje
+
+  screen(voltage0, voltage1, voltage2, voltage3);
+
+  
   // Esperar 500ms
   delay(500);
 }
