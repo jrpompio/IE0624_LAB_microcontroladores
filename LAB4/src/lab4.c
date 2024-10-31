e81bb1e1fbd1539a0b4f8fb1b92cd67937d88f6c
jrpompio <junior.ruiz@ucr.ac.cr>
2024-10-30 21:12:12 -0600

    Comunicación serial
    Signed-off-by: jrpompio <junior.ruiz@ucr.ac.cr>

--- LAB4/src/lab4.c
+++ LAB4/src/lab4.c
@@ -1,6 +1,15 @@
 #include <libopencm3/stm32/rcc.h>
 #include <libopencm3/stm32/gpio.h>
 #include <libopencm3/stm32/spi.h>
+#include <libopencm3/stm32/usart.h>
+#include <errno.h>
+#include <stdio.h>
+#include <unistd.h>
+
+#define USART_CONSOLE USART1
+#define SPI_BUS SPI5
+
+int _write(int file, char *ptr, int len);
 
 static void clock_setup(void) {
     rcc_clock_setup_pll(&rcc_hse_8mhz_3v3[RCC_CLOCK_3V3_168MHZ]);
@@ -25,7 +34,33 @@ static void usart_setup(void) {
     usart_enable(USART_CONSOLE);
 }
 
-void main(void) {
+int _write(int file, char *ptr, int len) {
+    int i;
+
+    if (file == STDOUT_FILENO || file == STDERR_FILENO) {
+        for (i = 0; i < len; i++) {
+            if (ptr[i] == '\n') {
+                usart_send_blocking(USART_CONSOLE, '\r');
+            }
+            usart_send_blocking(USART_CONSOLE, ptr[i]);
+        }
+        return i;
+    }
+    errno = EIO;
+    return -1;
+}
 
 
+
+
+int main(void) {
+    clock_setup();
+    usart_setup();
+    while (1)
+    {
+        printf("hola mundo\n");
+    }
+    
+    
+    return 0;
 }
