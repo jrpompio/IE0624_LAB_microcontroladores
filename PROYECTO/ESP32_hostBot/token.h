7fddbbde835c1e06788f960c00f8a94383e2ea97
Rocha0104 <mariorocha200104@gmail.com>
2024-11-27 01:19:56 -0600

    Agregando codigo firmware del esp32
    Signed-off-by: Rocha0104 <mariorocha200104@gmail.com>

---
+++ PROYECTO/ESP32_hostBot/token.h
@@ -0,0 +1,6 @@
+const char* ssid = "Fala lalala";
+const char* password = "sopadefrijoles13";
+const String token = "7710126261:AAHGyqZFCY6cvUjaCMvTfmQep-YOwtb1SSQ";
+
+const int64_t user1 = 1478239051; // Junior
+const int64_t user2 = 1481725146; // Mario
